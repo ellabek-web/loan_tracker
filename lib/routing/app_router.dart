7d647350ff59759da5ca80b7229d9a9b89c63177
ellabek-web <eliasbw1992@gmail.com>
2025-05-16 14:30:33 +0300

    the second commit

--- lib/routing/app_router.dart
+++ lib/routing/app_router.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:loan_tracker/screens/home_page.dart';
 import 'package:loan_tracker/screens/login_page.dart';
+import 'package:loan_tracker/screens/signUp_page.dart';
 import 'package:loan_tracker/screens/splash';
 
 final GoRouter appRouter =GoRouter(
@@ -18,7 +19,10 @@ final GoRouter appRouter =GoRouter(
     GoRoute(path: '/home',
     name: 'home',
     builder: (context, state) => const HomePage(),),
-
+// /SignUpPage
+ GoRoute(path: '/SignUpPage',
+    name: 'SignUpPage',
+    builder: (context, state) => const SignUpPage(),),
 
   ]
   );
